1e8055c6c91e391e4c61661cc6602ace357de517
Shivani Tripurari <shivani@volopay.co>
2024-11-14 11:05:05 +0530

    basic table component is created

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -6,11 +6,18 @@ import Table from './Table'
 
 function App() {
   
+  const header = ['Company', 'Contact', 'Country'];
+  const data = [
+    { Company: 'Alfedro Fransis', Contact: 'Maria Jones', Country: 'USA' },
+    { Company: 'Smith and co', Contact: 'Shawn Trevor', Country: 'Canada' },
+    { Company: 'Green olives', Contact: 'Ben Illinois', Country: 'UK' },
+  ]
+
 
   return (
     <>
      <h1 className='text-5xl font-bold '>Table Component</h1>
-     <Table/>
+     <Table headers={header} data={data}/>
     </>
   )
 }
--- my-project/src/Table.jsx
+++ my-project/src/Table.jsx
@@ -1,8 +1,32 @@
 import PropTypes from "prop-types";
-const Table = (({header, data})=>{
+const Table = (({headers, data})=>{
     return(
         <>
         <h1 className="text-xl font-semibold">Just a demo</h1>
+        <table className=""> 
+            <thead>
+                {/* for the header */}
+                <tr>
+                    {headers.map((header, index)=>(
+                        <th key={index}>
+                            {header}
+                        </th>
+                    ))}
+                </tr>
+            </thead>
+            <tbody>
+                {/* for the body */}
+                {data.map((row,rowIndex)=>(
+                    <tr key={rowIndex}> 
+                        {headers.map((col, colIndex)=>(
+                            <td key={colIndex}> 
+                                {row[col]}
+                            </td>
+                        ))}
+                    </tr>
+                ))}
+            </tbody>
+        </table>
         </>
     )
 })
