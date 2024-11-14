09f4b87b95f32a10836cea194038dba5827978ab
Shivani Tripurari <shivani@volopay.co>
2024-11-14 10:46:07 +0530

    Created a basic Vite+tailwind structure

---
+++ my-project/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Table from './Table'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1 className='text-5xl font-bold '>Table Component</h1>
+     <Table/>
+    </>
+  )
+}
+
+export default App
---
+++ my-project/src/Table.jsx
@@ -0,0 +1,10 @@
+import PropTypes from "prop-types";
+const Table = (({header, data})=>{
+    return(
+        <>
+        <h1 className="text-xl font-semibold">Just a demo</h1>
+        </>
+    )
+})
+
+export default Table
