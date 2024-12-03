1d739513cce077d739537fbacd3ffb2e3e03d41b
Shivani Tripurari <shivani@volopay.co>
2024-12-03 09:58:38 +0530

    added comments

--- assignment2/src/store/reducers/productSlice.js
+++ assignment2/src/store/reducers/productSlice.js
@@ -2,14 +2,70 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const initialState = {
-  products: [],
-  categories: [],
-  selectedCategory: null,
-  pagination: { limit: 15, skip: 0, page: 1 },
+  products: [],  //for storing products 
+  categories: [], //for 5 categories used
+  selectedCategory: null, // the category we selected
+  pagination: { limit: 15, skip: 0, page: 1 }, // for infinite scroll
   loading: false,
   error: null,
   updatingProduct: null, // Prefill
 };
+
+// Slice
+const productSlice = createSlice({
+    name: 'product',
+    initialState,
+    reducers: {
+      setProducts: (state, action) => {
+        if (Array.isArray(action.payload)) {
+          state.products = [...action.payload];
+        } else {
+          state.products.push(action.payload);
+        }
+      },
+  
+      setCategories: (state, action) => {
+          state.categories = action.payload;
+        },
+        setSelectedCategory: (state, action) => {
+            state.selectedCategory = action.payload;
+            state.pagination.skip = 0;
+            state.pagination.page = 1;
+            state.products = [];
+        },
+        setLoading: (state, action) => {
+            state.loading = action.payload;
+        },
+        setError: (state, action) => {
+            state.error = action.payload;
+        },
+        incrementPage: (state) => {
+            state.pagination.page += 1;
+            state.pagination.skip = (state.pagination.page - 1) * state.pagination.limit;
+        },
+        resetProducts: (state) => {
+            state.products = [];
+        },
+        setAddedProduct: (state, action) => {
+            state.updatingProduct = action.payload;
+            state.products = [...state.products, state.updatingProduct];  // adding new products to the end of existing products
+        },      
+      setUpdatingProduct: (state, action) => {
+          state.updatingProduct = action.payload;
+          if (action.payload) {
+            const index = state.products.findIndex((pro) => pro.id === action.payload.id);
+            if (index !== -1) {
+              state.products[index] = {
+                ...state.products[index],
+                ...action.payload,
+              };
+            }
+          }
+        },
+        
+    },
+  });
+  
 
 // Fetch all products
 export const fetchAllProducts = createAsyncThunk(
@@ -113,62 +169,6 @@ export const addProduct = createAsyncThunk(
 
 
 
-// Slice
-const productSlice = createSlice({
-  name: 'product',
-  initialState,
-  reducers: {
-    setProducts: (state, action) => {
-      if (Array.isArray(action.payload)) {
-        state.products = [...action.payload];
-      } else {
-        state.products.push(action.payload);
-      }
-    },
-
-    setAddedProduct: (state, action) => {
-        // state.products = [...state.products, action.payload];
-        state.updatingProduct = action.payload;
-        state.products = [...state.products, state.updatingProduct];
-    },      
-    setCategories: (state, action) => {
-      state.categories = action.payload;
-    },
-    setSelectedCategory: (state, action) => {
-      state.selectedCategory = action.payload;
-      state.pagination.skip = 0;
-      state.pagination.page = 1;
-      state.products = [];
-    },
-    setLoading: (state, action) => {
-      state.loading = action.payload;
-    },
-    setError: (state, action) => {
-      state.error = action.payload;
-    },
-    incrementPage: (state) => {
-      state.pagination.page += 1;
-      state.pagination.skip = (state.pagination.page - 1) * state.pagination.limit;
-    },
-    resetProducts: (state) => {
-      state.products = [];
-    },
-    setUpdatingProduct: (state, action) => {
-        state.updatingProduct = action.payload;
-      
-        if (action.payload) {
-          const index = state.products.findIndex((pro) => pro.id === action.payload.id);
-          if (index !== -1) {
-            state.products[index] = {
-              ...state.products[index],
-              ...action.payload,
-            };
-          }
-        }
-      },
-      
-  },
-});
 
 export const {
   setProducts,
